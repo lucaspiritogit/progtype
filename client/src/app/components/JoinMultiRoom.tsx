fbab04a6fa8da94ea7d2d33e243cd889e4af461d
lucaspiritogit <lucasignaciopirito@gmail.com>
2024-01-28 16:52:04 -0300

    fixing env variable

--- client/src/app/components/JoinMultiRoom.tsx
+++ client/src/app/components/JoinMultiRoom.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const server  = process.env.WEBSOCKET_SERVER as string;
+
+const socket = io(server);
 
 const JoinMultiRoom = ({ showJoinRoom, setShowJoinRoom }: any) => {
   const [username, setUsername] = useState("");
