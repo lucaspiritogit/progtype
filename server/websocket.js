5ac9f3bf4f002c387d5cd8ac59a624b0f22fe5db
lucaspiritogit <lucasignaciopirito@gmail.com>
2024-02-09 14:58:52 -0300

    format backend and fix gitignore

--- server/websocket.js
+++ server/websocket.js
@@ -1,47 +1,45 @@
 const express = require("express");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = app.listen(PORT, ()=> console.log(`server on ${PORT}`))
+const server = app.listen(PORT, () => console.log(`server on ${PORT}`));
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN
-  }
+    origin: process.env.CORS_ORIGIN,
+  },
 });
-const connections = new Set()
+const connections = new Set();
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   console.log("New client connected " + socket.id);
-  connections.add(socket.id)
+  connections.add(socket.id);
 
-  socket.on("join-room", function(roomId) {
-    socket.join(roomId)
+  socket.on("join-room", function (roomId) {
+    socket.join(roomId);
     const room = io.sockets.adapter.rooms.get(roomId);
 
     if (room.size > 2) {
       socket.emit("room-full", { roomId });
-      socket.disconnect()
+      socket.disconnect();
     } else {
       socket.join(roomId);
-      console.log("Client: " + socket.id + " connected to room with id: " + roomId);
+      console.log(
+        "Client: " + socket.id + " connected to room with id: " + roomId
+      );
     }
+  });
 
-  })
+  socket.on("sendTypeValueP2", function (data) {
+    io.to(data.roomId).emit("receiveTypeValueP2", data);
+  });
 
-  socket.on("sendTypeValueP2", function(data) {
-      io.to(data.roomId).emit("receiveTypeValueP2", data);
-
-    });
-
-
-  socket.on("sendTypeValueP1", function(data) {
+  socket.on("sendTypeValueP1", function (data) {
     io.to(data.roomId).emit("receiveTypeValueP1", data);
-
   });
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected: " + socket.id)
-    connections.delete(socket.id)
-  })
+    console.log("Client disconnected: " + socket.id);
+    connections.delete(socket.id);
+  });
 });
